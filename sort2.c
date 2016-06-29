278ee1d7de5739eb43f97782c324c958c9b73e4e
Anita Paudel <anitapau@uw.edu>
2016-06-29 19:51:09 +0000

    added sort2

---
+++ sort2.c
@@ -0,0 +1,16 @@
+#include "sort2.h"
+//Selection sort
+void sort2(int[] theArray, int theNum) {
+ int i, min, j;
+ for(int i = 0; i < theNum; i++) {
+  min =i;
+  for( j = i+1; j < theNum; j++) {
+    if(theArray[min] > theArray[j]) {
+       min = j;
+    }
+  }
+  int  temp = theArray[i];
+   theArray[i] = theArray[min];
+   theArray[min] = temp;
+ }
+}
