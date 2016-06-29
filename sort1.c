bedbf10348e94bc2a2d232cc69625f57596f6758
Anita Paudel <anitapau@uw.edu>
2016-06-29 19:33:48 +0000

    modified sort1.c

--- sort1.c
+++ sort1.c
@@ -1,10 +1,10 @@
 #include "sort1.h"
 void bubbleSort(int[] theArray, int theNum) {
 int i, j, temp;
- for(i = 1; i< n;i++) {
+ for(i = 1; i< theNum;i++) {
    j= i;
    temp = theArray[i];
-   for(j = 0; j < n-1; j++) {
+   for(j = 0; j < theNum-1; j++) {
     if(theArraj[j] > theArray[j+1]) {
      temp = theArray[j];
      theArray[j] = theArray[j+1];
@@ -12,6 +12,6 @@ int i, j, temp;
      }
    }
 
-   n--;
+   theNum--;
  }
 }
